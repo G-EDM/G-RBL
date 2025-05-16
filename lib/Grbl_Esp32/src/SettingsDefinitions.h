5232c373edf67a316213abb183a30bfa7d5f2574
G-EDM <138816703+G-EDM@users.noreply.github.com>
2025-05-16 13:51:04 +0200

    Delete lib/Grbl_Esp32/src directory

--- lib/Grbl_Esp32/src/SettingsDefinitions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-extern AxisMaskSetting* homing_dir_mask;
-extern AxisMaskSetting* homing_squared_axes;
-extern AxisMaskSetting* homing_cycle[MAX_N_AXIS];
-extern FlagSetting* limit_invert;
-extern FlagSetting* soft_limits;
